4f922b0c86b43fd1750e41a0c36fc3debe211ef3
Lux <1763773457@qq.com>
2023-03-08 21:18:07 +0800

    master commit2

--- src/main/java/com/test/GitDemo.java
+++ src/main/java/com/test/GitDemo.java
@@ -6,5 +6,7 @@ public class GitDemo {
         System.out.println("Hello Git");
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
+        System.out.println("Hello master-test");
+
     }
 }
