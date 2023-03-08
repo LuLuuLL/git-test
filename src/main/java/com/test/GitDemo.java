02b33cc131a71d8ea2a1724937ef765277b1d68c
Lux <1763773457@qq.com>
2023-03-08 21:17:00 +0800

    hot-fix2 commit

--- src/main/java/com/test/GitDemo.java
+++ src/main/java/com/test/GitDemo.java
@@ -6,5 +6,6 @@ public class GitDemo {
         System.out.println("Hello Git");
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
+        System.out.println("Hello hot-fix");
     }
 }
