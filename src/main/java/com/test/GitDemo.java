36619a9c19df5bca1780ca0029a9fb8c593af6a7
Lux <1763773457@qq.com>
2023-03-08 21:04:04 +0800

    Second commit

--- src/main/java/com/test/GitDemo.java
+++ src/main/java/com/test/GitDemo.java
@@ -3,5 +3,6 @@ package com.test;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Git");
     }
 }
